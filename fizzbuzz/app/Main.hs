e004384711af642dd3c981d4ebe06d6ca4c808be
yuizho <yuizho3@gmail.com>
2026-05-16 22:52:13 +0900

    modify fizzbuzz

--- fizzbuzz/app/Main.hs
+++ fizzbuzz/app/Main.hs
@@ -1,15 +1,13 @@
 module Main where
 
-import Control.Monad (when)
-
 main :: IO ()
-main = loop 0
+main = foldr f (return ()) $ map fizzbuzz [0 .. 20]
   where
-    loop n
-      | n <= 20 = do
-          when (n `mod` 3 /= 0 && n `mod` 5 /= 0) (putStr $ show n)
-          when (n `mod` 3 == 0) (putStr "Fizz")
-          when (n `mod` 5 == 0) (putStr "Buzz")
-          putStrLn ""
-          loop (n + 1)
-      | otherwise = return ()
+    fizzbuzz n
+      | n `mod` 15 == 0 = "FizzBuzz"
+      | n `mod` 3 == 0 = "Fizz"
+      | n `mod` 5 == 0 = "Buzz"
+      | otherwise = show n
+    f str act = do
+      putStrLn str
+      act
