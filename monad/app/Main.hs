6bd67d5ee1ac6cf81bddb185fa2963f07a8c720c
yuizho <yuizho3@gmail.com>
2026-05-19 22:54:52 +0900

    modify monad demo to add state monad

--- monad/app/Main.hs
+++ monad/app/Main.hs
@@ -1,10 +1,24 @@
 module Main where
 
+import Control.Monad.Trans.State
 import Maybe.MaybeMonad
 
 printItem :: Maybe Item -> IO ()
 printItem Nothing = putStrLn " その商品は見つかりませんでした"
 printItem (Just item) = putStrLn $ show item
+
+increment :: State Int ()
+increment = do
+  n <- get
+  put (n + 1)
+
+counter :: State Int String
+counter = do
+  increment
+  increment
+
+  finalCount <- get
+  return ("最終的なカウント: " ++ show finalCount)
 
 main :: IO ()
 main = do
@@ -13,3 +27,10 @@ main = do
   printItem humberger
   let sushi = getItemWithMonad menu "Foods" "Sushi"
   printItem sushi
+
+  putStrLn "======================"
+
+  -- 5.1.3 Stateモナド
+  let (result, finalState) = runState counter 0
+  putStrLn result
+  putStrLn $ show finalState
