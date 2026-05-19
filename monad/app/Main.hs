bd2ec45ba19cd25cef66dc8332f6adfb7e6ce228
yuizho <yuizho3@gmail.com>
2026-05-20 01:27:58 +0900

    modify monad demo to add Sts monad

--- monad/app/Main.hs
+++ monad/app/Main.hs
@@ -1,6 +1,10 @@
 module Main where
 
+import Control.Monad (forM_)
+import Control.Monad.ST (runST)
+import Control.Monad.Trans.Reader (Reader, ask, runReader)
 import Control.Monad.Trans.State
+import Data.STRef (modifySTRef, newSTRef, readSTRef)
 import Maybe.MaybeMonad
 
 printItem :: Maybe Item -> IO ()
@@ -20,6 +24,30 @@ counter = do
   finalCount <- get
   return ("最終的なカウント: " ++ show finalCount)
 
+type Config = String
+
+getUrl :: Reader Config String
+getUrl = do
+  profile <- ask
+  if profile == "prod"
+    then return "https://example.com"
+    else return "http://localhost:8080"
+
+myApp :: Reader Config String
+myApp = do
+  -- getUrlだけでconfigが取れる
+  url <- getUrl
+  return ("接続先は " ++ url ++ " です")
+
+counterBySTs :: Integer
+counterBySTs = runST $ do
+  n <- newSTRef 0
+
+  forM_ [0 .. 5] $ \i -> do
+    modifySTRef n (+ i)
+
+  readSTRef n
+
 main :: IO ()
 main = do
   -- 5.1.2 Maybeモナド
@@ -34,3 +62,14 @@ main = do
   let (result, finalState) = runState counter 0
   putStrLn result
   putStrLn $ show finalState
+
+  putStrLn "======================"
+
+  -- 5.5 Readerモナド
+  let appResult = runReader myApp "prod"
+  putStrLn appResult
+
+  putStrLn "======================"
+
+  -- 5.6 Stsモナド
+  print counterBySTs
